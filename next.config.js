9a1040b4b173bd5ce315247b5d277b4f26d76913
ktqappi <kt@alphaguilty.io>
2025-06-07 20:31:06 +0600

    fix: revert config for next

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self' https://ipinfo.io; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
-          }
-        ],
-      },
-    ]
-  },
-  images: {
-    domains: ['ipinfo.io'],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
