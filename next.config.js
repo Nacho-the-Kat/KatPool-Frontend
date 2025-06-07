92c3163a278e2bf98c1a8bbaed4884cb9df06dfd
ktqappi <kt@alphaguilty.io>
2025-06-07 20:13:19 +0600

    fix: allow non-SSL requests for IP detection API

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self' http://ip-api.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+          }
+        ],
+      },
+    ]
+  },
+  images: {
+    domains: ['ip-api.com'],
+  },
+}
 
 module.exports = nextConfig
