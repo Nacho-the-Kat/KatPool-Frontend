e5a8e62c25f702750eb34fd90a0f8f5798d71e6c
ktqappi <kt@alphaguilty.io>
2025-06-20 20:57:11 +0600

    feat: added ui changes related to total paid kas-nacho for particular miner

--- app/api/pool/blocks24h/route.ts
+++ app/api/pool/blocks24h/route.ts
@@ -5,11 +5,6 @@ import logger from '@/lib/utils/logger'
 export const runtime = 'edge';
 export const revalidate = 10;
 
-interface BlockData {
-  block_hash: string;
-  [key: string]: string; // For the dynamic DAA score key and timestamp
-}
-
 export async function GET() {
   const headersList = headers();
   const traceId = headersList.get('x-trace-id') || undefined;
@@ -17,7 +12,7 @@ export async function GET() {
   try {
     const baseUrl = process.env.API_BASE_URL || 'http://kas.katpool.xyz:8080';
     const response = await fetch(
-      `${baseUrl}/api/pool/blockdetails?currentPage=1&perPage=1000`,
+      `${baseUrl}/api/pool/blockcount24h`,
       {
         headers: {
           'x-trace-id': traceId || '',
@@ -29,24 +24,11 @@ export async function GET() {
       throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
     }
 
-    const data = await response.json().then(data => data.data);
-    if (!data || !Array.isArray(data)) {
-      throw new Error('Invalid response format');
-    }
-
-    // Calculate timestamp for 24 hours ago
-    const twentyFourHoursAgo = Date.now() - (24 * 60 * 60 * 1000);
-
-    // Count blocks with timestamps in the last 24 hours
-    const blocks24h = data.reduce((count: number, block: BlockData) => {
-      const blockTimestamp = new Date(block.timestamp).getTime();
-      return blockTimestamp >= twentyFourHoursAgo ? count + 1 : count;
-    }, 0);
-
+    const data = await response.json();
     return NextResponse.json({
       status: 'success',
       data: {
-        totalBlocks24h: blocks24h
+        totalBlocks24h: Number(data.blockCount24h)
       }
     });
 
